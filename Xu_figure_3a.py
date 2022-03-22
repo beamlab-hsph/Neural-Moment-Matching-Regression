97a6ba1cf1aae138a3b19163782e7a3b6c59ad57
DavidBellamy <Cokmeqcifbukcifta5>
2022-03-22 18:58:46 -0400

    Replaced the NMMR Demand results with the appropriate experiment with W's generated from a separate distribution than the train dist (diff random seed)

--- Xu_figure_3a.py
+++ Xu_figure_3a.py
@@ -12,8 +12,8 @@ pmmr_1000 = pd.read_csv("/Users/dab1963/PycharmProjects/DeepFeatureProxyVariable
 pmmr_5000 = pd.read_csv("/Users/dab1963/PycharmProjects/DeepFeatureProxyVariable/dumps/03-21-21-16-20 PMMR Fig 3 Demand/n_sample:5000/result.csv", header=None, names=['pmmr_5000'])
 dfpv_1000 = pd.read_csv("/Users/dab1963/PycharmProjects/DeepFeatureProxyVariable/dumps/03-21-21-21-15 DFPV Fig 3 Demand/n_sample:1000/result.csv", header=None, names=['dfpv_1000'])
 dfpv_5000 = pd.read_csv("/Users/dab1963/PycharmProjects/DeepFeatureProxyVariable/dumps/03-21-21-21-15 DFPV Fig 3 Demand/n_sample:5000/result.csv", header=None, names=['dfpv_5000'])
-nmmr_1000 = pd.read_csv("/Users/dab1963/PycharmProjects/DeepFeatureProxyVariable/dumps/03-22-02-23-52 NMMR for Fig 3 Demand/n_sample:1000/result.csv", header=None, names=['nmmr_1000'])
-nmmr_5000 = pd.read_csv("/Users/dab1963/PycharmProjects/DeepFeatureProxyVariable/dumps/03-22-02-23-52 NMMR for Fig 3 Demand/n_sample:5000/result.csv", header=None, names=['nmmr_5000'])
+nmmr_1000 = pd.read_csv("/dumps/03-22-18-34-39 NMMR Fig 3 Demand/n_sample:1000/result.csv", header=None, names=['nmmr_1000'])
+nmmr_5000 = pd.read_csv("/dumps/03-22-18-34-39 NMMR Fig 3 Demand/n_sample:5000/result.csv", header=None, names=['nmmr_5000'])
 
 # Vertically concatenate 1000 & 5000 dataset size results
 df_1000 = pd.concat([kpv_1000, cevae_1000, pmmr_1000, dfpv_1000, nmmr_1000], axis=1, ignore_index=False)
