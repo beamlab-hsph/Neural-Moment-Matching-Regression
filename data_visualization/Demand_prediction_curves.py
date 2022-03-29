Fixed a hidden bug in the NMMR experiment: model's preds had a shape mismatch with y_struct vector, resulting in incorrect causal oos MSE calculation at test time.

data_visualization/Demand_prediction_curves.py
@@ -9,54 +9,55 @@ ticket_prices_fine = np.linspace(10, 30, 1000)
 ticket_prices_coarse = np.linspace(10, 30, 10)
 EY_doA = np.array([cal_structural(a) for a in ticket_prices_fine])
 
-sample_size_to_plot = 5000
-
-# cwd = os.getcwd()
-cwd = "/Users/dab1963/PycharmProjects/Neural-Moment-Matching-Regression/data_visualization"
+cwd = os.getcwd()
 data_for_figs = op.join(cwd, "data_for_demand_figures")
 method_dirs = next(os.walk(data_for_figs))[1]
 
-num_subplots = len(method_dirs)
-num_figure_rows = int(np.ceil(num_subplots / 3))
-fig, axs = plt.subplots(num_figure_rows, 3, sharey=True)
+sample_sizes_to_plot = [1000, 5000]
 
-# Delete any empty plots on the last row
-if num_subplots % 3 == 1:
-    fig.delaxes(axs[num_figure_rows - 1, 1])
-    fig.delaxes(axs[num_figure_rows - 1, 2])
-elif num_subplots % 3 == 2:
-    fig.delaxes(axs[num_figure_rows - 1, 2])
+for n in sample_sizes_to_plot:
 
-dot_plot = False
-for i, method_dir in enumerate(method_dirs):
-    x_coord = 0 + (i // 3)
-    y_coord = 0 + (i % 3)
+    num_subplots = len(method_dirs)
+    num_figure_rows = int(np.ceil(num_subplots / 3))
+    fig, axs = plt.subplots(num_figure_rows, 3, sharey=True)
 
-    method_name = method_dir.split()[0].lower()
-    method_dirpath = op.join(data_for_figs, method_dir)
-    result_path = op.join(method_dirpath, f"n_sample:{sample_size_to_plot}")
+    # Delete any empty plots on the last row
+    if num_subplots % 3 == 1:
+        fig.delaxes(axs[num_figure_rows - 1, 1])
+        fig.delaxes(axs[num_figure_rows - 1, 2])
+    elif num_subplots % 3 == 2:
+        fig.delaxes(axs[num_figure_rows - 1, 2])
 
-    # Plot the true EY_doA curve and prepare the subplot
-    axs[x_coord, y_coord].plot(ticket_prices_fine, EY_doA, color="black")
-    axs[x_coord, y_coord].set_title(f"{method_name}", fontsize=10)
+    dot_plot = False
+    for i, method_dir in enumerate(method_dirs):
+        x_coord = 0 + (i // 3)
+        y_coord = 0 + (i % 3)
 
-    for filename in os.listdir(result_path):
-        if filename.endswith('.txt'):
-            strings = open(os.path.join(result_path, filename)).read().split()
-            pred_EY_doA = np.array([float(x) for x in strings])
-            if dot_plot:
-                axs[x_coord, y_coord].scatter(ticket_prices_coarse, pred_EY_doA)
-            else:
-                axs[x_coord, y_coord].plot(ticket_prices_coarse, pred_EY_doA, linewidth=1, linestyle='dashed')
+        method_name = method_dir.split()[0].lower()
+        method_dirpath = op.join(data_for_figs, method_dir)
+        result_path = op.join(method_dirpath, f"n_sample:{n}")
 
-for ax in axs.flat:
-    ax.set(xlabel='Plane ticket price (A)')
+        # Plot the true EY_doA curve and prepare the subplot
+        axs[x_coord, y_coord].plot(ticket_prices_fine, EY_doA, color="black")
+        axs[x_coord, y_coord].set_title(f"{method_name}", fontsize=10)
 
-# fig.text(0, 0.5, 'Expected plane ticket sales: E[Y | do(A)]', va='center', rotation='vertical')
-fig.supylabel('Expected plane ticket sales: E[Y | do(A)]')
-fig.suptitle(f"Predictions of E[Y | do(A)] by each method (n={sample_size_to_plot})")
-for ax in axs.flat:
-    ax.label_outer()
+        for filename in os.listdir(result_path):
+            if filename.endswith('.txt'):
+                strings = open(os.path.join(result_path, filename)).read().split()
+                pred_EY_doA = np.array([float(x) for x in strings])
+                if dot_plot:
+                    axs[x_coord, y_coord].scatter(ticket_prices_coarse, pred_EY_doA)
+                else:
+                    axs[x_coord, y_coord].plot(ticket_prices_coarse, pred_EY_doA, linewidth=1, linestyle='dashed')
 
-plt.tight_layout()
-plt.show()
+    for ax in axs.flat:
+        ax.set(xlabel='Plane ticket price (A)')
+
+    # fig.text(0, 0.5, 'Expected plane ticket sales: E[Y | do(A)]', va='center', rotation='vertical')
+    fig.supylabel('Expected plane ticket sales: E[Y | do(A)]')
+    fig.suptitle(f"Predictions of E[Y | do(A)] by each method (n={n})")
+    for ax in axs.flat:
+        ax.label_outer()
+
+    plt.tight_layout()
+    plt.show()

src/models/NMMR/NMMR_experiments.py
@@ -27,8 +27,10 @@ class NMMR_Trainer(object):
 
         # TODO: send model layers to GPU/CUDA, change model init to accept them (see DFPV trainer)
 
-        self.writer = SummaryWriter(log_dir=op.join(dump_folder, "tensorboard_log"))
         self.mse_loss = nn.MSELoss()
+
+        if self.log_metrics:
+            self.writer = SummaryWriter(log_dir=op.join(dump_folder, "tensorboard_log"))
 
     def train(self, train_t: PVTrainDataSetTorch, val_t: PVTrainDataSetTorch,
               kernel_matrix_train, kernel_matrix_val, verbose: int = 0) -> MLP_for_demand:
@@ -136,8 +138,20 @@ def NMMR_demand_experiment(data_config: Dict[str, Any], model_param: Dict[str, A
 
     if test_data.structural is not None:
         # test_data_org.structural is equivalent to EY_doA
-        oos_loss: float = np.mean((pred - test_data_org.structural.squeeze()) ** 2)
+        np.testing.assert_array_equal(pred.shape, test_data_org.structural.shape)
+        oos_loss = np.mean((pred - test_data_org.structural) ** 2)
         if data_config["name"] in ["kpv", "deaner"]:
             oos_loss = np.mean(np.abs(pred.numpy() - test_data_org.structural.squeeze()))
-    np.savetxt(one_mdl_dump_dir.joinpath(f"{random_seed}.pred.txt"), pred)
     return oos_loss
+
+
+if __name__ == "__main__":
+    data_config = {"name": "demand", "n_sample": 5000}
+    model_param = {"name": "nmmr",
+                   "n_epochs": 1000,
+                   "batch_size": 1000,
+                   "gpu_flg": "False",
+                   "log_metrics": "False"}
+
+    one_mdl_dump_dir = Path(op.join("/Users/dab1963/PycharmProjects/Neural-Moment-Matching-Regression/dumps", "temp_new"))
+    NMMR_demand_experiment(data_config, model_param, one_mdl_dump_dir, random_seed=41, verbose=0)