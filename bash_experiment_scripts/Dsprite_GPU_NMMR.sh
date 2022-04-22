b3e73c16e024db4ffba3f85e4a66f1c477572db5
DavidBellamy <Cokmeqcifbukcifta5>
2022-04-21 22:18:04 -0400

    Added some dSprite config files for NMMR. Also created a visualization of the U-W and U-Z relationships.

--- bash_experiment_scripts/Dsprite_GPU_NMMR.sh
+++ bash_experiment_scripts/Dsprite_GPU_NMMR.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -c 1                               # Request one core
-#SBATCH -t 0-05:00                         # Runtime in D-HH:MM format
+#SBATCH -t 1-00:00                         # Runtime in D-HH:MM format
 #SBATCH -p gpu_beam                        # Partition to run in
 #SBATCH --gres gpu:a100:1                        # Number of gpus
 #SBATCH --mem=24g                       # Memory total in MiB (for all cores)
-#SBATCH -o slurm_logs/Dsprite_GPU.out                 # File to which STDOUT will be written, including job ID (%j)
-#SBATCH -e slurm_logs/Dsprite_GPU.err                 # File to which STDERR will be written, including job ID (%j)
+#SBATCH -o slurm_logs/Dsprite_GPU_%j.out                 # File to which STDOUT will be written, including job ID (%j)
+#SBATCH -e slurm_logs/Dsprite_GPU_%j.err                 # File to which STDERR will be written, including job ID (%j)
 #SBATCH --account=beam_ab455
 
-echo "Running NMMR dsprite"
-python main.py configs/nmmr_dsprite.json ate
+python main.py configs/nmmr_u_dsprite.json ate
+python main.py configs/nmmr_v_dsprite.json ate
