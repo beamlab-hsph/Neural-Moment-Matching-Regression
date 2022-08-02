50c1bfa21c0a8d9a49c2e4bbc864bff8e54e6919
Ben Kompa <ben.kompa@gmail.com>
2022-08-02 10:09:40 -0400

    NN_AWY noise config

--- bash_experiment_scripts/scratch_GPUbeam.sh
+++ bash_experiment_scripts/scratch_GPUbeam.sh
@@ -2,10 +2,10 @@
 #SBATCH -c 1                               # Request one core
 #SBATCH -t 3:00:00                        # Runtime in D-HH:MM format
 #SBATCH -p gpu_beam                      # Partition to run in
-#SBATCH --gres=gpu:a100:1                        # Number of gpus
+#SBATCH --gres=gpu:titanx:1                        # Number of gpus
 #SBATCH --mem=24g                       # Memory total in MiB (for all cores)
 #SBATCH -o slurm_logs/nmmr_u_demandnoise_%j.out                 # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e slurm_logs/nmmr_u_demandnoise_%j.err                 # File to which STDERR will be written, including job ID (%j)
 #SBATCH --account=beam_ab455
 
-python main.py configs/nmmr_v_demandnoise.json ate
+python main.py configs/demand_noise_configs/naivenet_aw_demandnoise.json ate
